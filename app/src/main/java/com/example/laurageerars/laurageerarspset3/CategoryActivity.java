3b0bd17864db8768e2f8e43cf770a1fd40aaa0d0
Laura Geerars <lauraisabeau@live.nl>
2017-11-17 21:37:27 +0100

    Changes in activities

--- app/src/main/java/com/example/laurageerars/laurageerarspset3/CategoryActivity.java
+++ app/src/main/java/com/example/laurageerars/laurageerarspset3/CategoryActivity.java
@@ -1,15 +1,17 @@
 package com.example.laurageerars.laurageerarspset3;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,29 +26,34 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-//In MainActivity worden de categories geregeld (maar dat is verwarrend met de naam CategoryActivity daar wordt Menu geregeld
-public class MainActivity extends AppCompatActivity {
-    public ArrayList<String> listcategory = new ArrayList<String>();
-    public ListView CategoryListView;
+//Er staat CategoryActivity maar dit bestand is voor het regelen van het menu van de verschillende categorieen
+public class CategoryActivity extends AppCompatActivity {
+    final public ArrayList<String> listmenu = new ArrayList<String>();
+    ListView MenuListView;
+    String CategoryMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        CategoryListView = (ListView) findViewById(R.id.theCategoryListView);
-
-        //Order button fixing
-        //Button orderbutton = (Button) findViewById(R.id.OrderButton);
-        //orderbutton.setOnClickListener(new OrderButtonClickListener());
+        setContentView(R.layout.activity_category);
+        //String[] test = {"test", "hoi"};
+        //Adapter(test);
+        MenuListView = (ListView) findViewById(R.id.theMenuListView);
 
         // code from: https://developer.android.com/training/volley/simple.html
 
         final TextView mTextView = (TextView) findViewById(R.id.textView);
 
+        Intent intent = getIntent();
+        CategoryMenu = (String) intent.getStringExtra("CategoryMenu");
+        mTextView.setText(CategoryMenu);
+
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://resto.mprog.nl/categories";
+        String url = "https://resto.mprog.nl/menu";
 
 
         // Request a string response from the provided URL.
@@ -56,25 +63,30 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         // Display the characters of the response string.
                         //mTextView.setText("Response is: " + response);
-
+                    if (response != null ) {
                         try {
 
                             JSONObject newObject = (JSONObject) new JSONTokener(response).nextValue();
-                            ArrayList<JSONObject> listcategory = new ArrayList<JSONObject>();
-                            JSONArray categoryArray = newObject.getJSONArray("categories");
-                            for (int i = 0; i < categoryArray.length(); i++) {
+                            //ArrayList<JSONObject> listmenu = new ArrayList<JSONObject>();
+                            JSONArray menuArray = newObject.getJSONArray("items");
+                            for (int i = 0; i < menuArray.length(); i++) {
                                 //listmenu.add(menuArray.getJSONObject(i));
                                 //mTextView.setText(menuArray.getJSONObject(i).getString("name"));
-                                addItem(categoryArray.get(i).toString());
+                                if (Objects.equals(menuArray.getJSONObject(i).getString("category"), CategoryMenu)) {
+                                    //addItem(menuArray.getJSONObject(i).getString("name"));
+                                    listmenu.add(menuArray.getJSONObject(i).getString("name"));
+                                }
                             }
-                            Adapter();
+                            //Toast.makeText(CategoryActivity.this, listmenu.toString(), Toast.LENGTH_LONG).show();
+                            Adapter(listmenu);
+
 
                         } catch (JSONException e) {
-                            //mTextView.setText(e.toString());
                             e.printStackTrace();
 
 
                         }
+                    }
 
                     }
 
@@ -90,35 +102,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void Adapter() {
+    public void Adapter(List Menu) {
         final TextView mTextView = findViewById(R.id.textView);
-        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listcategory);
-        ListView MenuListView = (ListView) findViewById(R.id.theCategoryListView);
+        //Log.d("test", "test1");
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Menu);
+        //Log.d("test", "test2");
+
+        ListView MenuListView = (ListView) findViewById(R.id.theMenuListView);
+        //Log.d("test", "test3");
+
         MenuListView.setAdapter(theAdapter);
+        //Log.d("test", "test4");
+
         MenuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                gotoCategoryMenu(String.valueOf(adapterView.getItemAtPosition(i)));
+                //gotoCategoryMenu(String.valueOf(adapterView.getItemAtPosition(i)));
+
 
             }
         });
     }
 
     public void addItem(String Item){
-
-        listcategory.add(Item);
+        listmenu.add(Item);
     }
 
-    public void gotoCategoryMenu(String CategoryMenu){
-        Intent intent = new Intent(this, CategoryActivity.class);
-        intent.putExtra("CategoryMenu", CategoryMenu);
+    public void addOrder(String Item){
+        SharedPreferences prefs = getSharedPreferences("order", MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString(Item, Item);
+        prefsEditor.commit();
+
+
+    }
+    public void gotoOrder(String Order){
+        Intent intent = new Intent(this, OrderActivity.class);
+        intent.putExtra("Order", Order);
         startActivity(intent);
     }
 
 
+
+
 }
-
-
-
 
 
